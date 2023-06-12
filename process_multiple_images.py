ad735e7284285542b107f68dd5de1d9d6ecd9194
malsem <37010237+malsem@users.noreply.github.com>
2023-06-13 01:04:13 +0800

    Update process_multiple_images.py
    Revert back to old codes since trying to do growth rate comparison was not a success yet

--- process_multiple_images.py
+++ process_multiple_images.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 from durian_plant_analysis import analyze_durian_plant
 
-# Define the image directory
-image_dir = "D:\Desktop\FYP\Datasets_Durian"
+# Define the directory containing the images
+image_dir = "D:\Desktop\FYP\duriantrees1"
 
 # Define the output file path
 output_file = "D:\Desktop\FYP\Datasets_Durian.xlsx"
@@ -14,24 +14,17 @@ column_names = ['image_name', 'canopy_size_cm', 'stem_size_cm', 'plant_height_cm
 # Create an empty DataFrame to store the results
 results_df = pd.DataFrame(columns=column_names)
 
-previous_measurements = []
-current_measurement = None
-
 # Loop over all the images in the directory
 for image_name in os.listdir(image_dir):
     # Load the image
     image_path = os.path.join(image_dir, image_name)
 
     # Analyze the durian plant in the image
-    result = analyze_durian_plant(image_path, previous_measurements, current_measurement)
+    result = analyze_durian_plant(image_path)
 
     # Add the result to the DataFrame
     result['image_name'] = image_name
-    results_df = results_df.append(result, ignore_index=True)
-
-    # Update previous_measurements and current_measurement
-    previous_measurements.append(result)
-    current_measurement = result
+    results_df = results_df._append(result, ignore_index=True)
 
 # Save the results to the output file
 results_df.to_excel(output_file, index=False)
